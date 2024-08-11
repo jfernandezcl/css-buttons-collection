edd5c007590e8fb1ab40e8aec56f2abd7cde7f81
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-11 18:30:36 +0200

    modified class and styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       <h1>Button collection</h1>
       <div>
         <button className="button-1">
-          <span className="button-text">Button-1</span>
           <img src="src/images/uiandroid.png" className="button-icon" />
+          Button-1
         </button>
       </div>
     </>
