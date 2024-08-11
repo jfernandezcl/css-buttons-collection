8ec8ab8607e2d9c0241c8aed2c241438a211e081
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-11 14:28:31 +0200

    created the images for the button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
     <>
       <h1>Button collection</h1>
       <div>
-        <button className="button-1">Button 1</button>
+        <button className="button-1">
+          Button 1
+          <img src="src/images/uiandroid.png" />
+        </button>
       </div>
     </>
   );
