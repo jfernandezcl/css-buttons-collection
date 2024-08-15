0c765eeea2a2879e7f072222147cacf795b704bb
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-15 16:08:13 +0200

    created the title class

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import Button1 from "./components/CodeButton1/Button1.jsx";
 function App() {
   return (
     <>
-      <h1>Button Collection</h1>
+      <h1 className="title-main">Button Collection</h1>
       <div>
         <Button1 />
-        {/* Puedes seguir agregando más botones aquí */}
       </div>
     </>
   );
