9cab10c908cd500fb97521ebf41205f7983eedf8
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-17 19:19:49 +0200

    created stated and html

---
+++ src/components/CodeButton2/Button2.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+
+
+const Button2 = () => {
+    const [showCode, setShowCode] = useState(false);
+    const [activeTab, setActiveTab] = useState('html');
+
+    const htmlCode = `
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button className="button-1">
+          <img src="src/images/uiandroid.png" className="button-icon" />
+          Button-1
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
+`;
+    const cssCode = ``;
+
+
+}
