14c410f4468f6c8e1c055b7d06e4fbde1a32264a
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-29 12:33:47 +0200

    button 3 styles

--- src/components/CodeButton3/Button3.jsx
+++ src/components/CodeButton3/Button3.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import "./Button3.css"
 
 const Button3 = () => {
-    const [showCode3, setShowCode3] = useState(false);
-    const [activeTab3, setActiveTab3] = useState('html');
+  const [showCode3, setShowCode3] = useState(false);
+  const [activeTab3, setActiveTab3] = useState('html');
 
-    const htmlCode = `
+  const htmlCode = `
 import "./App.css";
 
 function App() {
@@ -22,14 +22,26 @@ function App() {
 }
 `;
 
-    const cssCode = ``;
+  const cssCode = `
+    .button-3 {
+    display: flex;
+    align-items: center;
+    background-color: aquamarine;
+    border: none;
+    margin-top: 32px;
+    gap: 16px;
+    width: 190px;
+    height: 56px;
+    padding: 12px 24px;
+}
+    `;
 
-    return (
-        <div>
-            <button className="button-3">Button-3</button>
+  return (
+    <div>
+      <button className="button-3">Button-3</button>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Button3;
